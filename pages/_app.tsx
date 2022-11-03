f656c5d9ac6082f1616597114adf296dd8ca03b1
Junho Park <dwarfthema@gmail.com>
2022-11-03 10:01:48 +0900

    feat:all:featcher 정의

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{ fetcher: (url: string) => fetch(url).then((res) => res.json()) }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
