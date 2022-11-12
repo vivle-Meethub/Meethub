0c3e6e4ac59dd7584ad1bcdd829fad7133119482
recomplete <wantop@jbnu.ac.kr>
2022-11-12 23:21:50 +0900

    #19 feat:all:메인 페이지 영상 배경 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,49 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const [username, setUsername] = useState("");
 
-  const myLoader = ({ src, width, quality }: any) => {
-    return `https://avatars.githubusercontent.com/u/65522153?v=4`;
-  };
-
   return (
     <Layout seoTitle="Main">
-      <div className="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover bg-[url('https://source.unsplash.com/1L71sPT5XKc')]">
-        <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+
+      <div
+        className="jb-box"
+        style={{ width: '100%', height: '100%', overflow: 'hidden', margin: '0px auto', position: 'relative' }}
+
+      >
+        <video style={{ width: '100%' }} muted autoPlay loop>
+          <source src="video/bg-video.mp4" type="video/mp4" />
+          <strong>Your browser does not support the video tag.</strong>
+        </video>
+
+        <div
+          className="jb-text"
+          style={{ position: 'absolute', top: '35%', left:'35%', width: '50%', zIndex: 10, color: '#fff' }}
+
+        >
+        
+
           <div
+          style={{ marginTop: '-24px', textAlign: 'center',  color: '#000' }}
             id="profile"
-            className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-90 mx-6 lg:mx-0"
+            className="w-full lg:w-3/5 shadow-2xl bg-white opacity-90"
           >
             <div className="p-4 md:p-12 text-center lg:text-left">
-              <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center bg-[url('https://avatars.githubusercontent.com/u/65522153?v=4')]"></div>
+              <div className="block lg:hidden rounded-full  -mt-16 h-48 w-48 bg-cover bg-center bg-[url('https://avatars.githubusercontent.com/u/65522153?v=4')]"></div>
 
               <input
-                className="text-3xl font-bold pt-8 lg:pt-0"
+                className="focus:outline-none font-bold pt-8 lg:pt-0"
                 placeholder="username"
                 onChange={(e) => setUsername(e.target.value)}
               />
               <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
               {/* <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z"/></svg> What you do</p>
-			<p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"/></svg> Your Location - 25.0000° N, 71.0000° W</p> */}
-              <p className="pt-8 text-sm">
+			          <p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z"/></svg> Your Location - 25.0000° N, 71.0000° W</p> */}
+              <p className="font-bold pt-8 text-sm">
                 밋허브에서 검색해서 누가 얼마나 <br /> 허슬하고 있는지 확인해
                 보세요!
               </p>
@@ -93,36 +105,8 @@ const Home: NextPage = () => {
                     <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
                   </svg>
                 </a>
-                <a
-                  className="link"
-                  href="#"
-                  data-tippy-content="@unsplash_handle"
-                >
-                  <svg
-                    className="h-6 fill-current text-gray-600 hover:text-[#78e08f]"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Unsplash</title>
-                    <path d="M7.5 6.75V0h9v6.75h-9zm9 3.75H24V24H0V10.5h7.5v6.75h9V10.5z" />
-                  </svg>
-                </a>
-                <a
-                  className="link"
-                  href="#"
-                  data-tippy-content="@dribble_handle"
-                >
-                  <svg
-                    className="h-6 fill-current text-gray-600 hover:text-[#78e08f]"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Dribbble</title>
-                    <path d="M12 24C5.385 24 0 18.615 0 12S5.385 0 12 0s12 5.385 12 12-5.385 12-12 12zm10.12-10.358c-.35-.11-3.17-.953-6.384-.438 1.34 3.684 1.887 6.684 1.992 7.308 2.3-1.555 3.936-4.02 4.395-6.87zm-6.115 7.808c-.153-.9-.75-4.032-2.19-7.77l-.066.02c-5.79 2.015-7.86 6.025-8.04 6.4 1.73 1.358 3.92 2.166 6.29 2.166 1.42 0 2.77-.29 4-.814zm-11.62-2.58c.232-.4 3.045-5.055 8.332-6.765.135-.045.27-.084.405-.12-.26-.585-.54-1.167-.832-1.74C7.17 11.775 2.206 11.71 1.756 11.7l-.004.312c0 2.633.998 5.037 2.634 6.855zm-2.42-8.955c.46.008 4.683.026 9.477-1.248-1.698-3.018-3.53-5.558-3.8-5.928-2.868 1.35-5.01 3.99-5.676 7.17zM9.6 2.052c.282.38 2.145 2.914 3.822 6 3.645-1.365 5.19-3.44 5.373-3.702-1.81-1.61-4.19-2.586-6.795-2.586-.825 0-1.63.1-2.4.285zm10.335 3.483c-.218.29-1.935 2.493-5.724 4.04.24.49.47.985.68 1.486.08.18.15.36.22.53 3.41-.43 6.8.26 7.14.33-.02-2.42-.88-4.64-2.31-6.38z" />
-                  </svg>
-                </a>
+
+
                 <a
                   className="link"
                   href="#"
@@ -156,19 +140,9 @@ const Home: NextPage = () => {
               </div>
             </div>
           </div>
-
-          <div className="w-full lg:w-2/5">
-            <Image
-              src="https://avatars.githubusercontent.com/u/65522153?v=4"
-              className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block "
-              width={500}
-              height={500}
-              loader={myLoader}
-              alt="로딩중"
-            />
-          </div>
         </div>
-      </div>
+          </div>
+
     </Layout>
   );
 };
